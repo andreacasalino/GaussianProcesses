b7a822a57142a6fd98bd4fd1b038af00245a3fa8
Andrea Casalino <andrea.casalino@cloudnc.com>
2021-12-02 17:30:48 +0000

    implementing

--- src/header/GaussianProcess/TrainSet.h
+++ src/header/GaussianProcess/TrainSet.h
@@ -7,19 +7,47 @@
 
 #pragma once
 
+#include <GaussianProcess/components/InputOutputSizeAware.h>
 #include <GaussianUtils/TrainSet.h>
+#include <memory>
 
 namespace gauss::gp {
-    class TrainSet {
-    public:
+class TrainSet : public InputOutputSizeAware {
+public:
+  TrainSet(const TrainSet &o) = default;
+  TrainSet &operator=(const TrainSet &o) = default;
 
-        void push(const Eigen::VectorXd& input_sample, const Eigen::VectorXd& output_sample);
+  TrainSet(TrainSet &&o) = default;
+  TrainSet &operator=(TrainSet &&o) = default;
 
-        const gauss::TrainSet& GetSamplesInput() const { return input; };
-        const gauss::TrainSet& GetSamplesOutput() const { return output; };
+  TrainSet(const std::string &file_to_read, const std::size_t input_space_size);
 
-    private:
-        gauss::TrainSet input;
-        gauss::TrainSet output;
-    };
-}
+  template <typename CollectionInput, typename CollectionOutput>
+  TrainSet(const CollectionInput &input_samples,
+           const CollectionOutput &output_samples)
+      : input(input_samples), output(output_samples){};
+
+  TrainSet(const Eigen::VectorXd &initial_input_sample,
+           const Eigen::VectorXd &initial_output_sample)
+      : input(std::vector<Eigen::VectorXd>{initial_input_sample}),
+        output(std::vector<Eigen::VectorXd>{initial_output_sample}){};
+
+  void operator+=(const gauss::gp::TrainSet &o) {
+    input += o.GetSamplesInput();
+    output += o.GetSamplesOutput();
+  };
+
+  void addSample(const Eigen::VectorXd &input_sample,
+                 const Eigen::VectorXd &output_sample) {
+    input.addSample(input_sample);
+    output.addSample(output_sample);
+  };
+
+  const gauss::TrainSet &GetSamplesInput() const { return input; };
+  const gauss::TrainSet &GetSamplesOutput() const { return output; };
+
+private:
+  gauss::TrainSet input;
+  gauss::TrainSet output;
+};
+} // namespace gauss::gp

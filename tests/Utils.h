e22362f164ac27fc47c1249ee8ba05aad122fd76
Andrea Casalino <andrecasa91@gmail.com>
2021-12-20 15:09:42 +0100

    testing

--- tests/Utils.h
+++ tests/Utils.h
@@ -9,20 +9,5 @@ public:
   GaussianProcessTest()
       : GaussianProcessBase(std::make_unique<LinearFunction>(1, 1), InputSize,
                             OutputSize){};
-
-protected:
-  TrainSet make_samples(const std::size_t samples_numb) {
-    std::vector<Eigen::VectorXd> input_samples;
-    input_samples.reserve(samples_numb);
-    std::vector<Eigen::VectorXd> output_samples;
-    output_samples.reserve(samples_numb);
-    for (std::size_t s = 0; s < samples_numb; ++s) {
-      input_samples.emplace_back(InputSize);
-      input_samples.back().setRandom();
-      output_samples.emplace_back(OutputSize);
-      output_samples.back().setRandom();
-    }
-    return gauss::gp::TrainSet{input_samples, output_samples};
-  };
 };
 } // namespace gauss::gp::test

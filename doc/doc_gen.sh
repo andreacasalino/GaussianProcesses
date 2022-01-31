01e99b0aa787c32146ee9d1df484e5e9182e9ab4
Andrea Casalino <andrecasa91@gmail.com>
2022-01-31 22:31:24 +0100

    documenting

--- doc/doc_gen.sh
+++ doc/doc_gen.sh
@@ -1,6 +1,8 @@
 rm *.ps *.dvi *.aux *.toc *.idx *.ind *.ilg *.log *.out *.brf *.blg *.bbl *.lot *.lof
 
 pdflatex Gaussian_Process.tex
+bibtex   Gaussian_Process
+pdflatex Gaussian_Process.tex
 pdflatex Gaussian_Process.tex
 
 rm *.ps *.dvi *.aux *.toc *.idx *.ind *.ilg *.log *.out *.brf *.blg *.bbl *.lot *.lof
